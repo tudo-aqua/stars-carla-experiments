779fa2cca39eba0fc35f51f44766f7d4b9a660fc
Dominik Schmid <dominik.schmid@tu-dortmund.de>
2024-11-04 08:49:01 +0100

    Revert "Bump com.github.ajalt.clikt:clikt from 4.4.0 to 5.0.0 (#11)"
    This reverts commit 3037d767a1cd8f1d8547f8e1cc949ef2cfe0e93a.

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
   implementation(group = "tools.aqua", name = "stars-logic-kcmftbl", version = starsVersion)
   implementation(group = "tools.aqua", name = "stars-data-av", version = starsVersion)
   implementation(group = "tools.aqua", name = "stars-importer-carla", version = starsVersion)
-  implementation(group = "com.github.ajalt.clikt", name = "clikt", version = "5.0.0")
+  implementation(group = "com.github.ajalt.clikt", name = "clikt", version = "4.4.0")
 }
 
 spotless {
